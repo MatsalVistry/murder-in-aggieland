f9f9133491b4ec690ca441d5f10021061e7365b7
MatsalVistry <vatsalmistry4796@gmail.com>
2023-03-09 13:29:30 -0600

    sample

---
+++ sample.php
@@ -0,0 +1,19 @@
+<?php
+    $is_get = $_SERVER['REQUEST_METHOD'] == 'GET';
+    $is_post = $_SERVER['REQUEST_METHOD'] == 'POST';
+
+    if($is_post)
+    {
+        if($_POST['functionName'] == "print")
+        {
+            echo "Hello World Post!";
+        }
+    }
+    else
+    {
+        if($_GET['functionName'] == "print")
+        {
+            echo "Hello World Get!";
+        }
+    }
+?>
