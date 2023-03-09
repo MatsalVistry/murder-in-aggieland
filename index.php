8400ab05895fe145789cd570f117932510d14f66
MatsalVistry <vatsalmistry4796@gmail.com>
2023-03-09 13:50:02 -0600

    -

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    $dbconn = pg_connect("host=ec2-34-226-11-94.compute-1.amazonaws.com port=5432 dbname=d3pn0nhl7uusfl user=frlqbtgkemxcet password=d90286b7c870a5195ec5709a37a460b82d3ff8d339720460cd43d9192a7874c1");
+
     $is_get = $_SERVER['REQUEST_METHOD'] == 'GET';
     $is_post = $_SERVER['REQUEST_METHOD'] == 'POST';
 
@@ -7,6 +9,12 @@
         if($_POST['functionName'] == "print")
         {
             echo "Hello World Post!";
+        }
+        else if($_POST['functionName'] == "makeTable")
+        {
+            // make a table called test with row id and name
+            $query = "create table test (id serial, name varchar(255));";
+            $result = pg_query($dbconn, $query);
         }
     }
     else
