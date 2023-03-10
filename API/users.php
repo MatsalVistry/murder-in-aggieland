653aeb94eb75d342d54506bece77d90748c20f28
MatsalVistry <vatsalmistry4796@gmail.com>
2023-03-09 22:16:01 -0600

    current games

--- API/users.php
+++ API/users.php
@@ -42,7 +42,7 @@
             $result = pg_query($dbconn, $query);
             $row = pg_fetch_row($result);
 
-            // return json object with code and message, code 0 = success, code 1 = failure
+            // return json object with code and message, code 0 = success, code 1 = failure, also return user id
 
             $response;
 
@@ -50,14 +50,72 @@
             {
                 $response = array(
                     'code' => 0,
-                    'message' => 'Success'
+                    'message' => 'Success',
+                    'user_id' => $row[0]
                 );
             }
             else
             {
                 $response = array(
                     'code' => 1,
-                    'message' => 'Invalid Credentials'
+                    'message' => 'Failed to verify credentials'
+                );
+            }
+        }
+    }
+    else
+    {
+        // TABLE user_game_joiner (
+        //     id SERIAL PRIMARY KEY,
+        //     user_id INTEGER REFERENCES users(id),
+        //     game_id INTEGER REFERENCES game(id),
+        //     current_priority INTEGER
+        // );
+
+        // TABLE game (
+        //     id SERIAL PRIMARY KEY,
+        //     game_name VARCHAR(150),
+        //     creator_id INTEGER REFERENCES users(id),
+        //     intial_text VARCHAR(1000),
+        //     game_description VARCHAR(150),
+        // );
+        
+        
+        
+        if($_POST['functionName'] == "getCurrentGames")
+        {
+            $user_id = $_POST['user_id'];
+
+            $query = "SELECT * FROM user_game_joiner as ugj INNER JOIN game as g ON ugj.game_id = g.id WHERE ugj.user_id = '$user_id';";
+            $result = pg_query($dbconn, $query);
+            $rows = pg_fetch_all($result);
+
+            $response;
+
+            if($rows)
+            {
+                $response = array(
+                    'code' => 0,
+                    'message' => 'Success',
+                    'game_ids' => array(),
+                    'game_names' => array(),
+                    'game_descriptions' => array(),
+                    'current_priorities' => array()
+                );
+
+                foreach($rows as $row)
+                {
+                    array_push($response['game_ids'], $row['game_id']);
+                    array_push($response['game_names'], $row['game_name']);
+                    array_push($response['game_descriptions'], $row['game_description']);
+                    array_push($response['current_priorities'], $row['current_priority']);
+                }
+            }
+            else
+            {
+                $response = array(
+                    'code' => 1,
+                    'message' => 'Failed to get current games'
                 );
             }
 
