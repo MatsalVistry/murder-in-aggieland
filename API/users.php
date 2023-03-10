bb2e8fbafcfa21fafa8a691e5c65a30ca44c5d5e
MatsalVistry <vatsalmistry4796@gmail.com>
2023-03-09 22:01:24 -0600

    users.php

---
+++ API/users.php
@@ -0,0 +1,51 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    $dbconn = pg_connect("host=ec2-34-226-11-94.compute-1.amazonaws.com port=5432 dbname=d3pn0nhl7uusfl user=frlqbtgkemxcet password=d90286b7c870a5195ec5709a37a460b82d3ff8d339720460cd43d9192a7874c1");
+
+    $is_get = $_SERVER['REQUEST_METHOD'] == 'GET';
+    $is_post = $_SERVER['REQUEST_METHOD'] == 'POST';
+
+    if($is_post)
+    {
+        if($_POST['functionName'] == "addUser")
+        {
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $query = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password');";
+            $result = pg_query($dbconn, $query);
+        }
+        else if($_POST['functionName'] == "verifyCredentials")
+        {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password';";
+            $result = pg_query($dbconn, $query);
+            $row = pg_fetch_row($result);
+
+            // return json object with code and message, code 0 = success, code 1 = failure
+
+            $response;
+
+            if($row)
+            {
+                $response = array(
+                    'code' => 0,
+                    'message' => 'Success'
+                );
+            }
+            else
+            {
+                $response = array(
+                    'code' => 1,
+                    'message' => 'Invalid Credentials'
+                );
+            }
+
+            echo json_encode($response);
+        }
+    }
+?>
+
--- SQL/game.sql
+++ SQL/game.sql
@@ -4,13 +4,15 @@
 -- column 1: id - int, primary key, auto increment
 -- column 2: game_name - varchar(150)
 -- column 3: creator_id - varchar(150) - foreign key to user table id column
--- column 4: game_killer - varchar(150)
+-- column 4: intial_text - varchar(1000)
 -- column 5: game_description - varchar(150)
+
 
 CREATE TABLE game (
     id SERIAL PRIMARY KEY,
     game_name VARCHAR(150),
     creator_id INTEGER REFERENCES users(id),
-    game_description VARCHAR(150)
+    intial_text VARCHAR(1000),
+    game_description VARCHAR(150),
 );
 
