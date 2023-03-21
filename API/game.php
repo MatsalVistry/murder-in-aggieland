079800e70e038867ae35198164440c86f20b3fa4
MatsalVistry <vatsalmistry4796@gmail.com>
2023-03-21 16:30:43 -0400

    -

--- API/game.php
+++ API/game.php
@@ -1,12 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    exit;
-  }
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Origin');
+
    
     $dbconn = pg_connect("host=ec2-34-226-11-94.compute-1.amazonaws.com port=5432 dbname=d3pn0nhl7uusfl user=frlqbtgkemxcet password=d90286b7c870a5195ec5709a37a460b82d3ff8d339720460cd43d9192a7874c1");
 
--- API/users.php
+++ API/users.php
@@ -1,5 +1,14 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        exit;
+      }
+      
+      // Handle actual requests
+      header('Access-Control-Allow-Origin: *');
+      header('Content-Type: application/json');
     $dbconn = pg_connect("host=ec2-34-226-11-94.compute-1.amazonaws.com port=5432 dbname=d3pn0nhl7uusfl user=frlqbtgkemxcet password=d90286b7c870a5195ec5709a37a460b82d3ff8d339720460cd43d9192a7874c1");
 
     $is_get = $_SERVER['REQUEST_METHOD'] == 'GET';
